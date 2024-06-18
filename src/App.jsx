3925acb5cdb54c4bbb40bb4bc0548a754d1dede0
Daneska Liz <daneskaliz@gmail.com>
2024-06-18 17:07:24 -0400

    Agrega funcionalidad básica del formulario (lectura de campos y actualización de datos del componente padre), corrige renderizado de imágenes en diferentes dimensiones

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Navbar from './components/Navbar.jsx';
 import Banner from './components/Banner.jsx';
 import Category from './components/Category.jsx';
@@ -18,40 +18,76 @@ import soft_3 from './assets/soft_3.png';
 
 function App() {
   const [formOpen, setFormOpen] = useState(false);
-  const [formData, setFormData] = useState({ title: '', imageURL: '', description: '', videoURL: '', category: '' });
+  const [formData, setFormData] = useState({ title: '', imageURL: '', description: '', videoURL: '', category: 'Front End' });
+  const [selectedCardIndex, setselectedCardIndex] = useState(null);
   const openForm = () => setFormOpen(true);
-  const closeForm = () => setFormOpen(false);
-  const cards = [
-    { title: 'Variables en JS', imageURL: front_1, description: '', videoURL: '', category: 'Front End' },
-    { title: 'JavaScript', imageURL: front_2, description: '', videoURL: '', category: 'Front End' },
-    { title: 'Equipo Front End', imageURL: front_3, description: '', videoURL: '', category: 'Front End' },
-    { title: 'Spring Framework', imageURL: back_1, description: '', videoURL: '', category: 'Back End' },
-    { title: 'SQL vs NoSQL', imageURL: back_2, description: '', videoURL: '', category: 'Back End' },
-    { title: 'Java Enum', imageURL: back_3, description: '', videoURL: '', category: 'Back End' },
-    { title: 'Que son las Soft Skills', imageURL: soft_1, description: '', videoURL: '', category: 'Innovación y Gestión' },
-    { title: '7 Soft Skills mas deseadas', imageURL: soft_2, description: '', videoURL: '', category: 'Innovación y Gestión' },
-    { title: 'Metodologías Ágiles', imageURL: soft_3, description: '', videoURL: '', category: 'Innovación y Gestión' },
-  ];
+  const clearForm = () => setFormData({ title: '', imageURL: '', description: '', videoURL: '', category: 'Front End' });
+  const closeForm = () => {
+    clearForm();
+    setFormOpen(false);
+  };
+  const [cards, setCards] = useState([
+    { title: 'Variables en JS', imageURL: front_1, description: 'Video 1 serie Front End', videoURL: 'https://www.youtube.com/watch?v=PztCEdIJITY', category: 'Front End' },
+    { title: 'JavaScript', imageURL: front_2, description: 'Video 2 serie Front End', videoURL: 'https://www.youtube.com/watch?v=GJfOSoaXk4s', category: 'Front End' },
+    { title: 'Equipo Front End', imageURL: front_3, description: 'Video 3 serie Front End', videoURL: 'https://www.youtube.com/watch?v=rpvrLaBQwgg', category: 'Front End' },
+    { title: 'Spring Framework', imageURL: back_1, description: 'Video 1 serie Backend', videoURL: 'https://www.youtube.com/watch?v=t-iqt1b2qqk', category: 'Back End' },
+    { title: 'SQL vs NoSQL', imageURL: back_2, description: 'Video 2 serie Back End', videoURL: 'https://www.youtube.com/watch?v=cLLKVd5CNLc', category: 'Back End' },
+    { title: 'Java Enum', imageURL: back_3, description: 'Video 3 serie Backend', videoURL: 'https://www.youtube.com/watch?v=EoPvlE85XAQ', category: 'Back End' },
+    { title: 'Que son las Soft Skills', imageURL: soft_1, description: 'Video 1 serie Soft Skills', videoURL: 'https://www.youtube.com/watch?v=vhwspfvI52k', category: 'Innovación y Gestión' },
+    { title: '7 Soft Skills mas deseadas', imageURL: soft_2, description: 'Video 2 serie Soft Skills', videoURL: 'https://www.youtube.com/watch?v=YhR7Zp8NUzE', category: 'Innovación y Gestión' },
+    { title: 'Metodologías Ágiles', imageURL: soft_3, description: 'Video 1 serie Soft Skills', videoURL: 'https://www.youtube.com/watch?v=6N3OkLCfK-0', category: 'Innovación y Gestión' },
+  ]);
   
-  const categories = [
+  const [categories, setCategories] = useState([
     { title: 'Front End', color: 'sky-300', cards: [] },
     { title: 'Back End', color: 'green-500', cards: [] },
     { title: 'Innovación y Gestión', color: 'yellow-500', cards: [] },
-  ];
+  ]);
 
-  (function pushCardsIntoCategories() {
-    cards.forEach(card => {
-      const categoryIndex = categories.findIndex(category => category.title == card.category);
-      categories[categoryIndex].cards.push(card);
+  function updateCategories() {
+    const categoriesUpdated = categories.map(category => {
+      return { ...category, cards: cards.filter(card => card.category == category.title) };
     });
-  }())
+    setCategories(categoriesUpdated);
+  }
+
+  function updateCard(formUpdates) {
+    const currentCards = [...cards];
+    currentCards[selectedCardIndex] = formUpdates;
+    setCards(currentCards);
+    closeForm();
+  }
+
+  function selectCardForEdition(title) {
+    const cardIndex = cards.findIndex(card => card.title == title);
+    setselectedCardIndex(cardIndex);
+    setFormData(cards[cardIndex]);
+    openForm();
+  }
+
+  useEffect(() => updateCategories(), [cards])
 
   return (
     <>
-      <Navbar openForm={openForm} closeForm={closeForm} />
+      <Navbar openForm={openForm} />
       <Banner />
-      {formOpen == true ? <CardForm closeForm={closeForm} formData={formData} categories={categories} /> : ''}
-      { categories.map((category) => <Category category={category} key={category.title} />) }
+
+      { formOpen == true ? <CardForm
+      closeForm={closeForm}
+      formData={formData}
+      categories={categories}
+      selectedCardIndex={selectedCardIndex}
+      updateCard={updateCard}
+      cards={cards}
+      /> : ''}
+
+      { categories.map((category) => (
+        <Category
+        key={category.title}
+        category={category}
+        selectCardForEdition={selectCardForEdition}
+      />)
+      ) }
       <Footer />
     </>
   )
--- src/components/CardForm.jsx
+++ src/components/CardForm.jsx
@@ -1,9 +1,31 @@
-
+import { useEffect, useState } from "react";
 export default function CardForm(props) {
+    const [title, setTitle] = useState('');
+    const [category, setCategory] = useState('');
+    const [imageURL, setImageURL] = useState('');
+    const [videoURL, setVideoURL] = useState('');
+    const [description, setDescription] = useState('');
+
+    useEffect(() => {
+        if (props.selectedCardIndex !== null) {
+            setTitle(props.formData.title);
+            setCategory(props.formData.category);
+            setImageURL(props.formData.imageURL);
+            setVideoURL(props.formData.videoURL);
+            setDescription(props.formData.description);
+        }
+    }, [])
     const categoryNames = props.categories.map(category => category.title);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (props.selectedCardIndex !== null) props.updateCard({ title, category, imageURL, videoURL, description });
+        else props.createCard()
+    }
+
+
     return (
     <div id="myModal" className="fixed left-0 top-0 z-10 w-full h-full bg-black bg-opacity-45">
-        <div className="relative mt-24 mx-auto h-[550px] w-11/12 lg:w-3/5 md:w-4/5 p-10 border-2 border-blue-300 bg-blue-950 rounded-md">   
+        <div className="relative mt-12 mx-auto h-[550px] w-11/12 lg:w-3/5 md:w-4/5 p-10 border-2 border-blue-300 bg-blue-950 rounded-md">   
             <button
             type="button"
             className="close top-5 right-10 absolute text-2xl text-blue-500 hover:text-blue-300 font-bold"
@@ -11,34 +33,45 @@ export default function CardForm(props) {
                 x
             </button>
             <h3 className="text-3xl text-blue-300 font-bold mb-2 text-center">EDITAR CARD</h3>
-            <form className="flex flex-col sm:w-5/6 lg:w-2/3 mt-8 mx-auto">
-                <label className="text-white text-sm font-bold" htmlFor="titulo">Título</label>
+            <form className="flex flex-col sm:w-5/6 lg:w-2/3 mt-8 mx-auto" onSubmit={(event) => onSubmit(event)}>
+                <label className="text-white text-sm font-bold" htmlFor="title">Título</label>
                 <input
                 className="my-1 text-white bg-transparent rounded-md ring-1 ring-blue-300 focus:outline-none px-2 py-1"
                 type="text"
-                name="titulo"
-                value={props.formData.title}
+                name="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
                 />
-                <label className="text-white text-sm font-bold" htmlFor="categoria">Categoria</label>
-                <select className="my-1 text-white bg-transparent rounded-md ring-1 ring-blue-300 focus:outline-none px-2 py-1" name="categoria" value={props.formData.category}>
-                    { categoryNames.map((categoryName) => <option className="bg-blue-900 text-white">{categoryName}</option>)}
+                <label className="text-white text-sm font-bold" htmlFor="category">Categoria</label>
+                <select
+                className="my-1 text-white bg-transparent rounded-md ring-1 ring-blue-300 focus:outline-none px-2 py-1"
+                name="category"
+                value={category}
+                onChange={(e) => setCategory(e.target.value)}
+                >
+                    { categoryNames.map((categoryName) => <option key={categoryName} className="bg-blue-900 text-white">{categoryName}</option>)}
                 </select>
-                <label className="text-white text-sm font-bold" htmlFor="imagen">Imagen</label>
+                <label className="text-white text-sm font-bold" htmlFor="imageURL">Imagen</label>
                 <input
                 className="my-1 text-white bg-transparent rounded-md ring-1 ring-blue-300 focus:outline-none px-2 py-1"
                 type="url"
-                name="imagen"
-                value={props.formData.imageURL} />
-                <label className="text-white text-sm font-bold" htmlFor="video">Video</label>
+                name="image"
+                value={imageURL}
+                onChange={(e) => setImageURL(e.target.value)}
+                />
+                <label className="text-white text-sm font-bold" htmlFor="videoURL">Video</label>
                 <input className="my-1 text-white bg-transparent rounded-md ring-1 ring-blue-300 focus:outline-none px-2 py-1"
                 type="url"
-                name="video"
-                value={props.formData.videoURL} />
-                <label className="text-white text-sm font-bold" htmlFor="descripcion">Descripción</label>
+                name="videoURL"
+                value={videoURL}
+                onChange={(e) => setVideoURL(e.target.value)}
+                />
+                <label className="text-white text-sm font-bold" htmlFor="description">Descripción</label>
                 <textarea
                 className="my-1 text-white bg-transparent rounded-md ring-1 ring-blue-300 focus:outline-none px-2 py-1"
-                name="descripcion"
-                value={props.formData.description}
+                name="description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
                 />
                 <div className="flex justify-between items-center mt-8">
                     <button type="submit" className="inner-shadow rounded-md text-white border border-blue-500 px-3 py-1 w-32 hover:scale-105 transition-transform">
